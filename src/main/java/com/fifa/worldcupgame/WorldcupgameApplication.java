e07da555adf94d35bde53b690484f4bbed4d23b0
Robson Vieira <robson.ppmg@gmail.com>
2022-01-27 19:47:24 -0300

    primeiro commit

---
+++ src/main/java/com/fifa/worldcupgame/WorldcupgameApplication.java
@@ -0,0 +1,13 @@
+package com.fifa.worldcupgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fifa/worldcupgame/WorldcupgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifa.worldcupgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
