65020888b863f9023f273808fb08dd6676602e43
Robson Vieira <robson.ppmg@gmail.com>
2022-01-30 17:29:52 -0300

    pacotes

--- src/main/java/com/fifa/worldcupgame/WorldcupgameApplication.java
+++ src/main/java/com/fifa/worldcupgame/WorldcupgameApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorldcupgameApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorldcupgameApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorldcupgameApplication.class, args);
+    }
 
 }
